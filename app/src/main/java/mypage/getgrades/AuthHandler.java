956527af60af0c6d4202fa1675633f28533c4b85
Humam Altayeb <homi.92@gmail.com>
2016-04-13 06:54:15 -0700

    @13Apr6.53AM

--- app/src/main/java/mypage/getgrades/AuthHandler.java
+++ app/src/main/java/mypage/getgrades/AuthHandler.java
@@ -1,15 +1,22 @@
 package mypage.getgrades;
 import java.io.IOException;
 import java.net.Proxy;
+import java.util.Iterator;
+import java.util.Map;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.FormElement;
 import org.jsoup.select.Elements;
 
 
@@ -19,6 +26,9 @@ public class AuthHandler extends AsyncTask<String, Void, String> {
     String userName, passWord;
     //UVic login page
     String URL = "https://www.uvic.ca/cas/login?service=https%3a//www.uvic.ca/BAN2P/banuvic.gzcaslib.P_Service_Ticket%3ftarget=bwskogrd.P_ViewTermGrde";
+    String URL2 = "https://www.uvic.ca/BAN2P/bwskogrd.P_ViewTermGrde";
+
+
 
     public AuthHandler(String userName, String passWord)
     {
@@ -38,10 +48,14 @@ public class AuthHandler extends AsyncTask<String, Void, String> {
                     .method(Connection.Method.GET)
                     .timeout(10000)
                     .execute();
+
             //Parse login form to retrieve the unique UVic LT value and execution value
             Document doc = loginForm.parse();
+
             String ltValue = doc.select("input[name=lt]").attr("value");
             String executionValue = doc.select("input[name=execution]").attr("value");
+            Map<String, String> loginCookies = loginForm.cookies();
+
             //Https Post with username, password, LT value, execution value,event ID and login cookies
             doc = Jsoup.connect(URL)
                     .data("username", userName)
@@ -49,9 +63,12 @@ public class AuthHandler extends AsyncTask<String, Void, String> {
                     .data("lt", ltValue)
                     .data("execution", executionValue)
                     .data("_eventId", "submit")
+                    .data("term_in", "201509")
+                    .referrer("https://www.uvic.ca/BAN2P/bwskogrd.P_ViewTermGrde")
                     .cookies(loginForm.cookies())
                     .post();
-            Log.d("String", doc.toString());
+            //Map<String, String> loginCookies = loginForm.cookies();
+            Log.d("String2", doc.toString());
             //String mWelcomeMessage = doc.select("a.glblLnK").html();
             String mWelcomeMessage = doc.title();
             if (mWelcomeMessage.equals("Term Grades"))
@@ -60,12 +77,20 @@ public class AuthHandler extends AsyncTask<String, Void, String> {
                 //goToGradesActivity();
                 Log.v(mWelcomeMessage, doc.toString());
                 //result = "proceed";
-                URL = "https://www.uvic.ca/BAN2P/bwskogrd.P_ViewTermGrde";
-                doc = Jsoup.connect(URL)
-                        .data("submit", "submit")
-                        .cookies(loginForm.cookies())
-                        .post();
-                Log.v("String", doc.toString());
+                //Document myDoc = loginForm.parse();
+
+
+                //Element table = doc.select("table[class=datadisplaytable tablesorter]").first();
+
+                //Iterator<Element> ite = table.select("td[class=default]").iterator();
+
+                //String mConfirm = doc.select("table[id=grades-table]").html();
+                //String mConfirm = doc.select("th.ddheader").html();
+                //mConfirm = table.size();
+
+                //Log.v("The mconfim", mConfirm);
+                Log.v("String3", doc.toString());
+                result = "proceed";
                 //return doc
             }
                 //Log.v(mWelcomeMessage, doc.toString());}
@@ -86,10 +111,4 @@ public class AuthHandler extends AsyncTask<String, Void, String> {
         //else Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
         return result;
     }
-
-   // private void goToGradesActivity()
-   // {
-      //  Intent intent = new Intent(this, Grades.class);
-      //  startActivity(intent);
-   // }
 }
