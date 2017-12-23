1b11e238fd8ba5b72585ea083efb1ba5fe970ce0
haltayeb <|Yqu4m6Dym>
2017-12-23 01:05:11 -0800

    @TESTV231217

--- app/src/main/java/mypage/getgrades/AuthHandler.java
+++ app/src/main/java/mypage/getgrades/AuthHandler.java
@@ -2,8 +2,12 @@ package mypage.getgrades;
 import java.io.IOException;
 
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.TextView;
 
 
 import org.jsoup.Connection;
@@ -12,79 +16,69 @@ import org.jsoup.nodes.Document;
 
 
 
-public class AuthHandler extends AsyncTask<String, Void, String> {
-
-    //username and password passed in https POST
-    String userName;
-    String passWord;
-    //UVic login page
-    String URL = "https://www.uvic.ca/cas/login?service=https%3a//www.uvic.ca/BAN2P/banuvic.gzcaslib.P_Service_Ticket%3ftarget=bwskogrd.P_ViewTermGrde";
+    public class AuthHandler extends AsyncTask<String, Void, String> {
 
 
-    public AuthHandler(String userName, String passWord)
-    {
-        // TODO Auto-generated constructor stub
-        this.userName = userName;
-        this.passWord = passWord;
-    }
-   //hello svn2
-    @Override
-    protected String doInBackground(String... params)
-    {
-        String result = "null";
-
-        try
-        {
-            //Connect to the UVic login page inorder to parse the login page
-            Connection.Response loginForm = Jsoup.connect(URL)
-                    .method(Connection.Method.GET)
-                    .timeout(10000)
-                    .execute();
-
-            //Parse login form to retrieve the unique UVic LT value and execution value
-            Document doc = loginForm.parse();
-            Log.d("String6", doc.toString());
-            String ltValue = doc.select("input[name=lt]").attr("value");
-            String executionValue = doc.select("input[name=execution]").attr("value");
+        //username and password passed in https POST
+        String userName;
+        String passWord;
 
 
-            //Https Post with username, password, LT value, execution value,event ID and login cookies
-            doc = Jsoup.connect(URL)
-                    .data("username", userName)
-                    .data("password", passWord)
-                    .data("lt", ltValue)
-                    .data("execution", executionValue)
-                    .data("_eventId", "submit")
-                    .cookies(loginForm.cookies())
-                    .ignoreHttpErrors(true)
-                    .post();
-            int statusCode = loginForm.statusCode();
-            //Map<String, String> loginCookies = loginForm.cookies();
-            Log.d("String2", doc.toString());
-            //String mWelcomeMessage = doc.select("a.glblLnK").html();
-            String mWelcomeMessage = doc.title();
+        //UVic login page
+        String URL = "https://www.uvic.ca/cas/login?service=https%3a//www.uvic.ca/BAN2P/banuvic.gzcaslib.P_Service_Ticket%3ftarget=bwskogrd.P_ViewTermGrde";
 
-            if (mWelcomeMessage.equals("Term Grades"))
-            {
 
-                Log.v(mWelcomeMessage, doc.toString());
+        public AuthHandler(String userName, String passWord) {
+            // TODO Auto-generated constructor stub
+            this.userName = userName;
+            this.passWord = passWord;
+        }
+        //hello svn2
 
-                result = "proceed";
+        @Override
+        protected String doInBackground(String... params) {
 
+            String result = "null";
+
+            try {
+                //Connect to the UVic login page inorder to parse the login page
+                Connection.Response loginForm = Jsoup.connect(URL).method(Connection.Method.GET).timeout(10000).execute();
+
+                //Parse login form to retrieve the unique UVic LT value and execution value
+                Document doc = loginForm.parse();
+                Log.d("String6", doc.toString());
+                String ltValue = doc.select("input[name=lt]").attr("value");
+                String executionValue = doc.select("input[name=execution]").attr("value");
+
+
+                //Https Post with username, password, LT value, execution value,event ID and login cookies
+                doc = Jsoup.connect(URL).data("username", userName).data("password", passWord).data("lt", ltValue).data("execution", executionValue).data("_eventId", "submit").cookies(loginForm.cookies()).ignoreHttpErrors(true).post();
+                int statusCode = loginForm.statusCode();
+                //Map<String, String> loginCookies = loginForm.cookies();
+                Log.d("String2", doc.toString());
+                //String mWelcomeMessage = doc.select("a.glblLnK").html();
+                String mWelcomeMessage = doc.title();
+                Log.v("mWelcomeMessage", mWelcomeMessage);
+                if (mWelcomeMessage.equals("Term Grades")) {
+
+                    Log.v(mWelcomeMessage, doc.toString());
+
+                    result = "proceed";
+                    Log.v("result", result);
+
+                } else {
+                    Log.v("MOOOOOOO", doc.toString());
+                    result = "error";
+                    Log.v("result", result);
+                }
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-            else
-            {
-                Log.v("MOOOOOOO", doc.toString());
-                result = "error";
-            }
-
-
-        } catch (IOException e)
-        {
-            e.printStackTrace();
+            return result;
         }
 
-        return result;
+
     }
-}
--- app/src/main/java/mypage/getgrades/MainActivity.java
+++ app/src/main/java/mypage/getgrades/MainActivity.java
@@ -1,8 +1,10 @@
 package mypage.getgrades;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,14 +16,16 @@ import android.widget.Toast;
 
 import java.util.concurrent.ExecutionException;
 
+import io.rmiri.buttonloading.ButtonLoading;
+
 public class MainActivity extends android.app.Activity
 {
     public static final String TAG = MainActivity.class.getSimpleName();
     TextView outputText;
-    Button sendData;
+//    Button sendData;
     EditText edtUser, edtPass;
     String URL = "https://www.uvic.ca/cas/login?service=https%3A%2F%2Fwww.uvic.ca%2F";
-
+    ButtonLoading buttonLoading;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -29,16 +33,53 @@ public class MainActivity extends android.app.Activity
         setContentView(R.layout.activity_main);
 
         outputText = (TextView) findViewById(R.id.textView1);
-        sendData = (Button) findViewById(R.id.button1);
+//        sendData = (Button) findViewById(R.id.button1);
         edtUser = (EditText) findViewById(R.id.editText1);
         edtPass = (EditText) findViewById(R.id.editText2);
-        sendData.setOnClickListener(new View.OnClickListener()
-        {
+        buttonLoading = (ButtonLoading) findViewById(R.id.btnDown);
+
+        buttonLoading.setOnButtonLoadingListener(new ButtonLoading.OnButtonLoadingListener() {
+            @Override
+            public void onClick() {
+                buttonLoading.setProgress(true);
+
+                authenticate();
+            }
 
             @Override
-            public void onClick(View v)
-            {
-                // TODO Auto-generated method stub
+            public void onStart() {
+
+            }
+
+
+
+
+            @Override
+            public void onFinish() {
+
+            }
+        });
+
+
+    }
+
+
+
+    private void goToGradesActivity(String userName, String passWord)
+    {
+        Intent intent = new Intent(this, Grades.class);
+        intent.putExtra("userName",userName);
+        intent.putExtra("passWord",passWord);
+        startActivity(intent);
+    }
+
+
+
+    private void authenticate() {
+        //call setProgress(false) after 5 second
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
                 String userName = edtUser.getText().toString();
                 String passWord = edtPass.getText().toString();
                 AuthHandler handler = new AuthHandler(userName, passWord);
@@ -72,44 +113,12 @@ public class MainActivity extends android.app.Activity
                 }
                 else
                 {
-                   Toast.makeText(getApplicationContext(), "Something went wrong! Restart the app!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Something went wrong! Restart the app!", Toast.LENGTH_SHORT).show();
                     //outputText.append(result + "\n");
                 }
                 Log.v(TAG, String.valueOf(result));
-
+                buttonLoading.setProgress(false);
             }
-        });
-
-    }
-
-    private void goToGradesActivity(String userName, String passWord)
-    {
-        Intent intent = new Intent(this, Grades.class);
-        intent.putExtra("userName",userName);
-        intent.putExtra("passWord",passWord);
-        startActivity(intent);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        }, 2000);
     }
 }
